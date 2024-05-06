107c65e9c242dd6aeb90f2a2c02593e4c1941854
Jay Gala <jaygala260@gmail.com>
2024-05-06 15:29:29 +0530

    add llama-cpp for faster inference

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ with sidebar:
         save_folder = 'uploaded_files/'
         save_path = os.path.join(save_folder, uploaded_file.name)
 
-        print(f"SAVE PATH: {save_path}")
         with open(save_path, mode='wb') as w:
             w.write(uploaded_file.getvalue())
 
@@ -66,9 +65,36 @@ if st.session_state.file_parsed:
             "prompt": prompt
         }
 
-        import json
+        import json, time
 
-        response = requests.post(url=url, data=json.dumps(data)).text
+        start_time = time.time()
+
+        from llama_cpp import Llama
+
+        llm = Llama(
+            model_path="./models\llama-2-7b.Q4_0.gguf",
+            n_gpu_layers=-1, # Uncomment to use GPU acceleration
+            # seed=1337, # Uncomment to set a specific seed
+            # n_ctx=2048, # Uncomment to increase the context window
+        )
+        response = llm(
+            f"""
+            Q: You are a helpful AI assistant. Help in answering the following query based on the given contexts.
+
+            Context: {chunks[idx]}
+
+            Query: {prompt}
+
+            A: """, # Prompt
+            max_tokens=32, # Generate up to 32 tokens, set to None to generate up to the end of the context window
+            stop=["Q:", "\n"], # Stop generating just before the model would generate a new question
+            echo=True # Echo the prompt back in the output
+        ) # Generate a completion, can also call create_completion
+        # print(output)
+
+        # response = requests.post(url=url, data=json.dumps(data)).text
+
+        st.info(f"time taken for response {time.time() - start_time}")
 
         with st.chat_message("assistant"):
 
---
+++ llama_1.py
@@ -0,0 +1,23 @@
+from llama_cpp import Llama
+
+llm = Llama(
+            model_path="./models/llama-2-7b.Q4_0.gguf",
+            # n_gpu_layers=-1, # Uncomment to use GPU acceleration
+            # seed=1337, # Uncomment to set a specific seed
+            # n_ctx=2048, # Uncomment to increase the context window
+        )
+
+print(llm)
+response = llm(
+            f"""
+            Q: You are a helpful AI assistant. Help in answering the following query based on the given contexts.
+
+            A: """, # Prompt
+            max_tokens=32, # Generate up to 32 tokens, set to None to generate up to the end of the context window
+            stop=["Q:", "\n"], # Stop generating just before the model would generate a new question
+            echo=True # Echo the prompt back in the output
+        ) # Generate a completion, can also call create_completion
+        # print(output)
+
+        # response = requests.post(url=url, data=json.dumps(data)).text
+print(response)
